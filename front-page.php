00e5d3ff4e47919a5f0d89f58f236036d9834804
Darrell Richards <hello@darrellrichards.com>
2016-01-22 11:57:25 -0500

    Added Styling and content to homepage.

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<section id="bottomNavBar">
+    	<h2 class="hide">Bottom Nav Bar</h2>
+    	<div class="row show-for-large-up">
+        	<div class="large-4 columns">
+            	<p><a href="results_trucks_race2.html">BREAKING: Wright Wins Atlanta!</a></p>
+            </div>
+        </div>
+    </section>
+<?php get_footer(); ?>
