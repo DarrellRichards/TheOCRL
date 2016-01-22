bfa85d28176b51b12c32f70e81d827be7b898446
Darrell Richards <hello@darrellrichards.com>
2016-01-22 11:44:45 -0500

    Updated Header with NAVBAR and updated footer links.

---
+++ footer.php
@@ -0,0 +1,10 @@
+	<script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.1.1/foundation.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/top-bar.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,13 +1,161 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="no-js" lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php bloginfo('title'); ?></title>
-	<link rel="stylesheet" href='<?php bloginfo('template_directory'); ?>/css/main.css'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.1.1/foundation.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/webicons/2.0.0/webicons.min.css" />
+    <link rel="stylesheet" href='<?php bloginfo('template_directory'); ?>/css/main.css'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 </head>
 <body>
-	
-</body>
-</html>
+
+
+<div class="row collapse"><!--start mobile area-->
+        	<div class="small-12 medium-12 columns hide-for-large-only"><!--start mobile nav-->
+            <nav class="top-bar" data-topbar role="navigation">
+  				<ul class="title-area">
+    				<li class="name">
+                    <a href="index.html">
+                    <h1><img id="mobileLogo" src="http://www.upperedgeracing.com/images/UpperEdge_Racing_Official.svg" alt="UpperEdge Logo"></h1>
+                    </a>
+    				</li>
+
+    			<li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+  				</ul>
+                	
+                    <section class="top-bar-section">
+                    <h2 class="hide">Top Bar Nav</h2>
+                    
+                    <ul id="mobileNav">
+                    	<li><a href="news.html">NEWS</a></li>
+                        <li><a href="video.html">VIDEO</a></li>
+                        <li class="has-dropdown"><a href="schedule-sportsman.html">SCHEDULE & RESULTS</a>
+                        		<ul class="dropdown">
+          						<li><a href="schedule-sportsman.html">SPORTSMAN</a></li>
+                                <li><a href="schedule-trucks.html">TRUCKS</a></li>
+                                <li><a href="schedule-super-late-model.html">SUPER LATE MODEL</a></li>
+                              </ul>
+                        </li>
+                        <li class="has-dropdown"><a href="standings-sportsman.php">STANDINGS</a>
+                        		<ul class="dropdown">
+          						<li><a href="standings-sportsman.php">SPORTSMAN</a></li>
+                                <li><a href="standings-trucks.php">TRUCKS</a></li>
+                                <li><a href="standings-super-late-model.php">SUPER LATE MODEL</a></li>
+                              </ul>
+                        </li>
+                        <li class="has-dropdown"><a href="drivers-sportsman.html">DRIVERS</a>
+                        		<ul class="dropdown">
+          						<li><a href="drivers-sportsman.html">SPORTSMAN</a></li>
+                                <li><a href="drivers-trucks.html">TRUCKS</a></li>
+                                <li><a href="drivers-super-late-model.html">SUPER LATE MODEL</a></li>
+                              </ul>
+                        </li>
+                        <li class="has-dropdown"><a href="track-records.html">STATS</a>
+                        		<ul class="dropdown">
+          						<li><a href="track-records.html">TRACK RECORDS</a></li>
+                                <li><a href="league-records.html">LEAGUE RECORDS</a></li>
+                              </ul>
+                        </li>
+                        <li class="has-dropdown"><a href="about.html">DISCOVER</a>
+                        		<ul class="dropdown">
+          						<li><a href="about.html">ABOUT</a></li>
+                                <li><a href="contact.php">CONTACT</a></li>
+                                <li><a href="register.php">REGISTER</a></li>
+                                <li><a href="rule-books.html">RULE BOOKS</a></li>
+                                <li><a href="champions.html">PAST CHAMPIONS</a></li>
+                              </ul>
+                        </li>
+                	</ul>
+                   
+                   </section> 
+                    
+			</nav>
+        	</div><!--close mobile nav-->
+         </div><!--close mobile area-->
+         
+  	<div id="topBar">
+    	<h2 class="hide">Top Bar</h2>
+    	<div class="row show-for-large-up">
+        	<div class="large-12 columns" id="topBarContent">
+    		<ul>
+        		<li><a href="power-rankings.html">POWER RANKINGS</a></li>
+            	<li><a href="winslist.html">ALL TIME WINS LIST</a></li>
+            	<li><a href="race-view.html">RaceCenter LIVE</a></li>
+            	<li><a href="partnership.html">PARTNERSHIP INFORMATION</a></li>
+            	<li><a href="super-late-model-clash.html">THE SUPER LATE MODEL CLASH</a></li>
+            	<li><a href="specialevents.html">SPECIAL EVENTS</a></li>
+            	<li><a href="seriesinfo.html">SERIES INFORMATION</a></li>
+        	</ul>
+            </div>
+        </div>
+    </div>
+    
+  	<header id="mainHeader"><!--Open Main Header-->
+    	<h2 class="hide">Main Header</h2>
+        
+        <div class="row show-for-large-up"><!--Open Desktop Nav-->
+        	<h2 class="hide">Logo</h2>
+    		<div class="large-4 column">
+            	<a href="index.html">
+            	<img src="http://www.upperedgeracing.com/images/UpperEdge_Racing_Official.svg" alt="UpperEdge Racing Logo" id="logo">
+                </a>
+            </div>
+            
+    		<nav id="mainNav" class="show-for-large-up large-8 column">
+            <h2 class="hide">Main Navigation</h2>
+            	<ul>
+					<li><a href="news.html">NEWS</a></li>
+ 					<li><a href="video.html">VIDEO</a></li>
+  	
+    			<li>
+    			<a href="schedule-sportsman.html">SCHEDULE & RESULTS</a>
+                    <ul>
+                      <li><a href="schedule-sportsman.html">SPORTSMAN</a></li>
+                      <li><a href="schedule-trucks.html">TRUCKS</a></li>
+                      <li><a href="schedule-super-late-model.html">SUPER LATE MODEL</a></li>
+                      <li><a href="includes/3_Wide_Schedule.pdf" target="blank">3 WIDE SCHEDULE</a></li>
+                    </ul>
+
+                <li>
+                <a href="standings-sportsman.html">STANDINGS</a>
+                    <ul>
+                      <li><a href="standings-sportsman.html">SPORTSMAN</a></li>
+                      <li><a href="standings-trucks.html">TRUCKS</a></li>
+                      <li><a href="standings-super-late-model.html">SUPER LATE MODEL</a></li>
+                    </ul>
+
+                <li>
+                <a href="drivers-sportsman.html">DRIVERS</a>
+                    <ul>
+                      <li><a href="drivers-sportsman.html">SPORTSMAN</a></li>
+                      <li><a href="drivers-trucks.html">TRUCKS</a></li>
+                      <li><a href="drivers-super-late-model.html">SUPER LATE MODEL</a></li>
+                      <li><a href="teams.html">TEAMS</a></li>
+                    </ul>
+
+                <li>
+                <a href="track-records.html">STATS</a>
+                    <ul>
+                      <li><a href="track-records.html">TRACK RECORDS</a></li>
+                      <li><a href="league-records.html">LEAGUE RECORDS</a></li>
+                    </ul>
+
+                <li>
+                <a href="about.html">DISCOVER</a>
+                    <ul>
+                      <li><a href="about.html">ABOUT</a></li>
+                      <li><a href="contact.html">CONTACT</a></li>
+                      <li><a href="register.html">REGISTER</a></li>
+                      <li><a href="rule-books.html">RULE BOOKS</a></li>
+                      <li><a href="champions.html">PAST CHAMPIONS</a></li>
+                    </ul>
+                </li>
+            </ul>
+          </nav>
+        </div><!--Close Desktop Nav-->
+        
+    </header><!--Close Main Header-->
