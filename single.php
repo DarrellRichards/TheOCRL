c41723f8f9218836e20fad758164eed8a929149b
Darrell Richards <hello@darrellrichards.com>
2016-01-29 12:31:01 -0500

    Started layout of single post, also added social links to share the post.

--- single.php
+++ single.php
@@ -1,31 +1,27 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<section class="list-blog" id="page_wrapper">
+	<section id="single_post">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-7">
 					<h2 class="headline"><?php the_title(); ?></h2>
 					<div class="content blog-content">
 						<p class="home_date">
-							<i class="fa fa-calendar"></i>  <?php the_time("j M 'y") ?>
-							<i class="fa fa-user"></i> <?php the_author(); ?>
-							<i class="fa fa-tags"></i> <?php echo get_the_tag_list(' ',',',''); ?></p>
-						<hr>
+							<?php the_time("M j Y") ?> |
+							<?php the_author(); ?>
+						</p>
 						<?php the_content(); ?>
-						<hr>
 					</div>
 				</div>
 				<div class="col-md-5">
 					<div class="content">
 						<aside class="sidebar">
-							<h4>Sidebar Coming Soon!</h4>
+							<h4>Widget Area Soon.</h4>
 						</aside>
 					</div>
 				</div>
 			</div>		
 <?php endwhile; ?>
-			<?php previous_post_link(); ?> 
-			<?php next_post_link(); ?>
 		</div>
 	</section>
 <?php else: ?>
